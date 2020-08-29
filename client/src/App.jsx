d2dfdfefb065b58de7fe6c284f38f270b41f6223
banhmisg9509 <banhmisg9509@users.noreply.github.com>
2020-08-29 15:34:35 +0700

    clean up, navbar, route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { NavBar, Home, About } from 'components';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as NavBar} from './layouts/NavBar/NavBar'
+export { default as Home} from './pages/Home/Home'
+export { default as About} from './pages/About/About'
