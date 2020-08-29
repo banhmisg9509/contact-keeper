75c925f8ab432d3ff1722d50370120c31153cbd5
banhmisg9509 <banhmisg9509@users.noreply.github.com>
2020-08-29 16:13:45 +0700

    Contacts Context global state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,23 @@ import { NavBar, Home, About } from 'components';
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { ContactState } from 'context';
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <NavBar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 }
 
---
+++ client/src/context/index.js
@@ -0,0 +1,2 @@
+export { default as ContactState } from './contact/contactState';
+export { default as ContactContext } from './contact/contactContext';
