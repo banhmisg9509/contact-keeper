714bcc300639e5be6b2c154243a1a1fbc2250a44
banhmisg9509 <banhmisg9509@users.noreply.github.com>
2020-08-30 15:36:19 +0700

    Auth Context inital state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,25 @@ import { NavBar, Home, About } from 'components';
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import { ContactState } from 'context';
+import { ContactState, AuthState } from 'context';
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <NavBar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
--- client/src/context/index.js
+++ client/src/context/index.js
@@ -1,2 +1,5 @@
-export { default as ContactState } from './contact/contactState';
+export { default as AuthContext } from './auth/authContext';
+export { default as AuthState } from './auth/authState';
+
 export { default as ContactContext } from './contact/contactContext';
+export { default as ContactState } from './contact/contactState';
